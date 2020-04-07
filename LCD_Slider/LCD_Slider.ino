a51c13ecacd6cd697384e5c9715c882d1dd58913
Kimchico <45200859+Kimchico@users.noreply.github.com>
2020-04-07 13:15:11 +0200

    Update LCD_Slider.ino

--- LCD_Slider/LCD_Slider.ino
+++ LCD_Slider/LCD_Slider.ino
@@ -1,15 +1,30 @@
-
+#include <DFPlayerMini_Fast.h>
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(10, 11);
+///
 #include <LiquidCrystal.h>
 
 int potPin = 1;
 int potPin2 = 2;
-int pauseBut = 4;
-int playBut = 5;
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(12, 9, 5, 4, 3, 2);
+
+int button = 8;
+int button2 = 7;
+int selectedTrack;
+DFPlayerMini_Fast myMP3;
 
 void setup() {
-    Serial.begin(9600); //this is the rate for the serial monitor, can be changed, doesn't matter much.
+    Serial.begin(115200); //this is the rate for the serial monitor, can be changed, doesn't matter much.
     lcd.begin(16, 2); //This is the LCD (Collums, Rows) so max 16 letters per row.
+
+    pinMode(button, INPUT);
+    pinMode(button2, INPUT);
+    mySerial.begin (9600);
+    myMP3.begin(mySerial);
+    delay(1000);
+    myMP3.volume(30);
+    delay(20);
+
 }
 void loop() {
   int valPotLCD = 0;
@@ -17,27 +32,30 @@ void loop() {
   //From here we treshhold values from the slider to display text on the LCD.
   //However due to a 1k resistor being attached to the slider, the first threshold is larger
   //Than the other... IDK if this is why, but my best guess. (It shoudl change around 1/3 of the way for each.
-    if (valPotLCD > 0 && valPotLCD < 800){
+    if (valPotLCD > 0 && valPotLCD < 869){
+      selectedTrack = 1;
       lcd.setCursor(0, 1);
-      Serial.println("interval 1");
-      lcd.print("David is a dick  ");
+      lcd.print("Adventure track ");
   }
-    if (valPotLCD > 801 && valPotLCD < 900){
+    if (valPotLCD > 869 && valPotLCD < 1023){
+      selectedTrack = 2;
       lcd.setCursor(0, 1);
-      Serial.println("interval 2");
-      lcd.print("David is a cunt  ");
+      lcd.print("Journey track   ");
   }
-    if (valPotLCD > 901 && valPotLCD < 1023){
-      lcd.setCursor(0, 1);
-      Serial.println("interval 3");
-      lcd.print("David is a twat ");
+  //Serial.print(selectedTrack);
+
+  if (digitalRead(button) == LOW){
+//      myMP3.play(selectedTrack);
+      Serial.print("LOW ");   
   }
-  //This is for testing the rotary pots
-  valPotLCD = analogRead(potPin2);
-  if (valPotLCD > 901 && valPotLCD < 1023){
-      lcd.setCursor(0, 0);
-      Serial.println("Pot Test");
-      lcd.print("David is a Javid");
+  if (digitalRead(button) == HIGH){
+//      myMP3.play(selectedTrack);
+      Serial.print("HIGH "); 
   }
-  Serial.println();
+//  if (digitalRead(button2) == LOW){
+//    myMP3.reset();
+//    Serial.print("reset");
+//     }
+lcd.setCursor(0, 0);
+lcd.print(valPotLCD);
 }
