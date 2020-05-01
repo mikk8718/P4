a323e81ae4c8e8390b9964273c9abce451fa516b
dmocko18 <50201406+dmocko18@users.noreply.github.com>
2020-05-01 14:02:34 +0200

    plays either song 1 or 2

--- Main_project/buttonPlay.py
+++ Main_project/buttonPlay.py
@@ -1,3 +1,33 @@
-import scipy
+import sounddevice as sd
+import scipy.io.wavfile as wave
+import keyboard
+import msvcrt
 
-print("Rhinos are just older unicorns")
+
+while True:
+    if msvcrt.kbhit():
+        key_stroke = msvcrt.getch()
+        print(key_stroke)
+samplingFreq = 44100
+
+
+def buttonPlay(play):
+    if play == 1:
+        samplingFreq, signal = wave.read("0001.wav")
+    if play == 2:
+        samplingFreq, signal = wave.read("0002.wav")
+
+
+
+    sd.play(signal, samplingFreq)
+    sd.wait()
+
+
+def buttonStop():
+    sd.stop()
+
+
+buttonPlay(1)
+
+
+
