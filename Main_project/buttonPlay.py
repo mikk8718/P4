f449d3dbc88e6268ee2b63fed359eb3c00746890
dmocko18 <50201406+dmocko18@users.noreply.github.com>
2020-05-01 15:59:56 +0200

    the buttonPlay function now has GUI for debugging

--- Main_project/buttonPlay.py
+++ Main_project/buttonPlay.py
@@ -3,27 +3,51 @@ import scipy.io.wavfile as wave
 import keyboard
 import msvcrt
 import os
+from tkinter import *
+
+
+# GUI stuff for debugging
+root = Tk()
+root.geometry("400x400") #size of the window
+###############
+
 
 #samplingFreq = 44100
 samplingFreq, signal = wave.read("testfile.wav")
 
 
-def buttonPlay(play):
-    if play == 1:
+def buttonPlay():
+    #if play == 1: These three lines will be used later
     #    applyEcho(sample, signal, echoStr)
     #    applyPitch(sample, signal, pitchStr)
 
         sd.play(signal, samplingFreq)
         # sd.wait()
 
-def buttonStop(state):
-    if state == 2:
+def buttonStop():
+    #if state == 2: Will be used
         sd.stop()
-        # os.remove("selectedTrack.wav")
+        try:
+            os.remove("selectedTrack.wav")
+        except:
+            print("nothing to delete")
 
 
-buttonPlay(1)
-buttonStop(2)
+# Created a GUI to see if it works and it indeed does so this chunk can and will be deleted later
+btnFrame = LabelFrame(root, text="Notes", padx=5, pady=5)
+btnFrame.pack(fill=X)
+btn1 = Button(btnFrame, text="Play", padx=20, pady=30, command=buttonPlay)  # fg="white", bg="black",
+btn1.pack(side=LEFT)
+btn2 = Button(btnFrame, text="Stop", padx=20, pady=30, command=buttonStop)
+# btn2.grid(row=btnRow, column=1)
+btn2.pack(side=LEFT)
+root.mainloop()
+#################################################################################
+
+# buttonPlay(1)
+# buttonStop(2)
+
+
 
 
 
