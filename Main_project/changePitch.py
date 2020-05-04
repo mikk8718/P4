5cdc79dec218faa77291327f1f682944ecbdb9ca
mikk8718 <baunsgaard38@gmail.com>
2020-05-04 15:25:06 +0200

    ..

--- Main_project/changePitch.py
+++ Main_project/changePitch.py
@@ -2,16 +2,21 @@ import numpy as np
 import scipy.io.wavfile as wave
 import sounddevice as sd
 import matplotlib.pyplot as plt
-import scipy
+import numpy as np
+from scipy.fftpack import rfft, irfft, fftfreq, fft
+
+sf, s = wave.read("pitch.wav")
+def speedx(sound_array, factor):
+    """ Multiplies the sound's speed by some `factor` """
+    indices = np.round(np.arange(0, len(sound_array), factor))
+    indices = indices[indices < len(sound_array)].astype(int)
+    return sound_array[indices.astype(int)]
+
+sd.play(speedx(s, 2), sf)
+sd.wait()
 
 
-sf, s = wave.read("testfile.wav")
-s = s / max(s)
-plt.plot(s)
-plt.xlabel("samples[n]")
-plt.ylabel("aplitude")
-sd.play(s, sf)
-plt.show()
+
 
 
 
