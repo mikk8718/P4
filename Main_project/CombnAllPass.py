e014a1f7688538347a1da92a6ab1cc2e9a89882c
dmocko18 <50201406+dmocko18@users.noreply.github.com>
2020-05-04 14:36:54 +0200

    added comb and allpass filter updated

--- Main_project/CombnAllPass.py
+++ Main_project/CombnAllPass.py
@@ -21,9 +21,23 @@ def combfilter(inputSignal, samplingFreq, filterCoefficient, delay):
     return outputSignal/max(outputSignal)
 
 
-def allpassfilter():
-    return
+def allpassfilter(inputSignal, filterCoefficient):
+    # d = np.int(np.round(delay * samplingFreq))
+    signalLenght = np.size(inputSignal)
+    outputSignal = np.zeros(signalLenght, dtype='int16')
 
-sd.play(combfilter(signal, samplef, 0.5, 0.15), samplef)
-sd.wait()
+    for n in np.arange(signalLenght):
+        if n < 1:
+            outputSignal[n] = inputSignal[n]
+        else:
+            outputSignal[n] = filterCoefficient * inputSignal[n] + inputSignal[n - 1] - filterCoefficient * outputSignal[n - 1]
 
+    return outputSignal / max(outputSignal)
+
+
+# sd.play(combfilter(signal, samplef, 0.5, 0.15), samplef)
+# sd.wait()
+# sd.play(allpassfilter(signal, 0.5), samplef)
+# sd.wait()
+plt.plot(allpassfilter(signal, 0.5))
+plt.show()
