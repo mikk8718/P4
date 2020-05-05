e64cab8fc6f11feb41dd74fcbe59dc193f985671
dmocko18 <50201406+dmocko18@users.noreply.github.com>
2020-05-05 13:25:39 +0200

    pitch updated

--- Main_project/changePitch.py
+++ Main_project/changePitch.py
@@ -1,23 +1,11 @@
-import numpy as np
 import scipy.io.wavfile as wave
 import sounddevice as sd
-import matplotlib.pyplot as plt
 import numpy as np
-from scipy.fftpack import rfft, irfft, fftfreq, fft
 
 sf, s = wave.read("pitch.wav")
-def speedx(sound_array, factor):
-    """ Multiplies the sound's speed by some `factor` """
-    indices = np.round(np.arange(0, len(sound_array), factor))
-    indices = indices[indices < len(sound_array)].astype(int)
-    return sound_array[indices.astype(int)]
 
-sd.play(speedx(s, 2), sf)
-sd.wait()
-
-
-
-
-
+def pitch(inputSignal, factor):
+    indices = np.round(np.arange(0, len(inputSignal)-1, factor))
+    return inputSignal[indices.astype(int)]
 
 
