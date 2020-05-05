9a948d30725c5b2ef8dc3675152799365728d25f
Kimchico <45200859+Kimchico@users.noreply.github.com>
2020-05-05 10:10:49 +0200

    Update main.py

--- Main_project/main.py
+++ Main_project/main.py
@@ -1,28 +1,45 @@
 import serial
-import re
 import sounddevice as sd
 import scipy.io.wavfile as wave
+from createTrack import createTrack
+from buttonPlay import buttonPlay, buttonStop
 
 data = serial.Serial("COM3", 115200)
-samplingFrequency, signal = wave.read("testfile.wav")
 
 def readInput(string):
     temp = 0
-    if "playButton" in string:
+
+    if "selectedTrack" in string:
+        trackNumber = [int(i) for i in string if i.isdigit()]
+        print("selectedTrack = {}".format(trackNumber[0]))
+        createTrack(trackNumber[0])
+
+    elif "selectedEcho" in string:
+        trackNumber = [int(i) for i in string if i.isdigit()]
+        print("selectedEcho = {}".format(trackNumber[0]))
+        #Insert Echo here
+
+    elif "selectedPitch" in string:
+        trackNumber = [int(i) for i in string if i.isdigit()]
+        print("selectedPitch = {}".format(trackNumber[0]))
+        #Insert Pitch here
+
+    elif "selectedVol" in string:
+        trackNumber = [int(i) for i in string if i.isdigit()]
+        print("selectedVol = {}".format(trackNumber[0]))
+
+
+    elif "playButton" in string:
         temp = [int(i) for i in string if i.isdigit()]
         print("playButton = {}".format(temp[0]))
+        samplingFreq, signal = wave.read("selectedTrack.wav")
 
-        sd.play(signal, samplingFrequency)
-        sd.wait()
+        buttonPlay(signal, samplingFreq)
 
     elif "stopButton" in string:
         temp = [int(i) for i in string if i.isdigit()]
         print("stopButton = {}".format(temp[0]))
-
-    elif "selectedTrack" in string:
-        temp = [int(i) for i in string if i.isdigit()]
-        print("selectedTrack = {}".format(temp[0]))
-
+        buttonStop()
 
 while True:
 
