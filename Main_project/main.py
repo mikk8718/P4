47e6d95efc7664543296d952909b64589de4aaaa
Kimchico <45200859+Kimchico@users.noreply.github.com>
2020-05-07 09:37:44 +0200

    Update main.py

--- Main_project/main.py
+++ Main_project/main.py
@@ -1,53 +1,49 @@
 import serial
-import sounddevice as sd
 import scipy.io.wavfile as wave
+from applyReverb import applyReverb
+from changePitch import pitch
 from createTrack import createTrack
 from buttonPlay import buttonPlay, buttonStop
 
 data = serial.Serial("COM3", 115200)
+variables = {"reverb": 1,
+             "pitch" :1,
+             "volume" : 1}
 
 def readInput(string):
-    temp = 0
 
     if "selectedTrack" in string:
         trackNumber = [int(i) for i in string if i.isdigit()]
-        print("selectedTrack = {}".format(trackNumber[0]))
         createTrack(trackNumber[0])
 
-    elif "selectedEcho" in string:
-        trackNumber = [int(i) for i in string if i.isdigit()]
-        print("selectedEcho = {}".format(trackNumber[0]))
-        #Insert Echo here
+    elif "selectedReverb" in string:
+        reverbNumber = [int(i) for i in string if i.isdigit()]
+        variables["reverb"] = reverbNumber[0]
 
     elif "selectedPitch" in string:
-        trackNumber = [int(i) for i in string if i.isdigit()]
-        print("selectedPitch = {}".format(trackNumber[0]))
-        #Insert Pitch here
+        pitchNumber = [int(i) for i in string if i.isdigit()]
+        variables["pitch"] = pitchNumber[0]
 
     elif "selectedVol" in string:
-        trackNumber = [int(i) for i in string if i.isdigit()]
-        print("selectedVol = {}".format(trackNumber[0]))
-
+        volNumber = [int(i) for i in string if i.isdigit()]
+        variables["volume"] = volNumber[0]
 
     elif "playButton" in string:
-        temp = [int(i) for i in string if i.isdigit()]
-        print("playButton = {}".format(temp[0]))
+        print(variables)
         samplingFreq, signal = wave.read("selectedTrack.wav")
 
-        buttonPlay(signal, samplingFreq)
+        signal2 = applyReverb(signal, samplingFreq, variables.get("reverb"))
+        signal3 = pitch(signal2, variables.get("pitch"))
+
+        buttonPlay(signal3, samplingFreq)
 
     elif "stopButton" in string:
-        temp = [int(i) for i in string if i.isdigit()]
-        print("stopButton = {}".format(temp[0]))
         buttonStop()
 
 while True:
 
     line = data.readline()
     string = line.decode("utf-8")
-    #print(type(string))
-    #res = [int(i) for i in string if i.isdigit()]
-    #print(string)
     readInput(string)
 
 
